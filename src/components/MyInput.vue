614b56363a43184f74fc7f173ecea35277d36f93
nganori <ngan.tran@axonactive.com>
2020-09-10 10:41:03 +0700

    Create new component

---
+++ src/components/MyInput.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <label>{{label}}</label>
+        <input type="text" :value="value" @input="$emit('input', $event.target.value)">
+        <span>Output: {{value}}</span>
+    </div>
+</template>
+<script lang = "ts">
+export default {
+    props: ["label", "value"]
+}
+</script>
